f044edb10a57512f98d9f4c0b494251b998464af
Axmart <notaxmart@gmail.com>
2020-11-13 22:18:48 +0100

    Create Fuvar class and set properties

---
+++ Fuvar/Fuvar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuvar
+{
+    class Fuvar
+    {
+
+        public int TaxiAzonosito { get; set; }
+        public DateTime IndulasIdeje { get; set; }
+        public int IdotartamMasodpercben { get; set; }
+        public double MegtettUtMerfoldben { get; set; }
+        public double ViteldijDollarban { get; set; }
+        public double BorravaloDollarban { get; set; }
+        public string FizetesModja { get; set; }
+
+
+    }
+}
