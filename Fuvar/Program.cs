3a6610c3d592427132bd188df7430cd0af2eeb29
Axmart <notaxmart@gmail.com>
2020-11-14 21:50:14 +0100

    3. Feladat

--- Fuvar/Program.cs
+++ Fuvar/Program.cs
@@ -13,10 +13,17 @@ namespace Fuvar
         static void Main(string[] args)
         {
             Fuvarok = Fuvar.Beolvasas();
-
+            UtazasokSzama();
 
 
             Console.ReadKey();
+
+        }
+
+        private static void UtazasokSzama()
+        {
+
+            Console.WriteLine($"3. Feladat: {Fuvarok.Count} fuvar");
 
         }
     }
