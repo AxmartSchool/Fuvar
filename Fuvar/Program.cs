73f15d5c53d3dd506203e6c749181f1c9bcfde96
Axmart <notaxmart@gmail.com>
2020-11-14 21:58:33 +0100

    4. Feladat

--- Fuvar/Program.cs
+++ Fuvar/Program.cs
@@ -14,9 +14,35 @@ namespace Fuvar
         {
             Fuvarok = Fuvar.Beolvasas();
             UtazasokSzama();
-
+            TaxisBevetele();
 
             Console.ReadKey();
+
+        }
+
+        private static void TaxisBevetele()
+        {
+
+            int fuvarokSzama = 0;
+            double osszBevetel = 0;
+
+            foreach (var fuvar in Fuvarok)
+            {
+
+                if (fuvar.TaxiAzonosito == 6185)
+                {
+                    fuvarokSzama++;
+                    osszBevetel += fuvar.ViteldijDollarban + fuvar.BorravaloDollarban;
+
+                }
+
+
+            }
+            //LINQ
+            //fuvarokSzama = Fuvarok.Count(x => x.TaxiAzonosito == 6185);
+
+            Console.WriteLine($"4. Feladat: {fuvarokSzama} fuvar alatt {osszBevetel} $");
+
 
         }
 
